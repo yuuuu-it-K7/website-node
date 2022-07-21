b95a632239a765c933796766fbfb02bb1243fa6d
川崎勇希 <yuuki@yuuki.local>
2022-07-21 23:20:39 +0900

    a

---
+++ app.js
@@ -0,0 +1,19 @@
+//expressの読み込み
+const express = require('express');
+//expressをを使用するための準備
+const app = express();
+
+//publicフォルダ内のファイルを読み込む
+app.use(express.static('public'));
+app.use(express.static('image'));
+
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form.ejs');
+});
+
+//localhost:3000でアクセス可能なサーバーを起動する
+app.listen(3000);
